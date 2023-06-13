3ee58b792a39730d44c9d7a1d2f2bb4b04b5565e
Aditya Narayan Mishra <Aditya.Mishra@e2open.com>
2023-06-13 22:30:48 +0530

    Added base Authentication

--- backend/README.md
+++ backend/README.md
@@ -1,2 +1,14 @@
 # Backend
 
+### Note: Rename the .env.example folder to .env and fill the URL and client ID detail mentioned in the sheet
+
+## Authentication
+
+-- backend - localhost:3000 (default)
+
+1. send get request to http://backend/login, user will login using their email or direct google/apple signin
+2. Once logged in it will redirect to /, where through success we can check if the user is successfully logged in or not. [Body may change in future for other data requirement]
+3. send get request to http://backend/profile, to get user details [Route and permission may change]
+4. send get request to http://backend/logout, to logout and check success message to successful logout
+5. Before sending any request to backend first check if user is logged in or not by calling http://backend and check the success message.[This url will be changed, Facing some issues at Auth0 service]
+6. Any non matched route will return this message -> `{"message":"Not Found","error":{"status":404}}`
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,62 @@
 const express = require('express');
 const app = express();
+require("dotenv").config();
+const logger = require("morgan");
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
-app.get('/status', (req, res) => {
-  res.status(200).send('OK');
+
+// Middlewares
+app.use(logger('dev'));
+app.use(express.json());
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true
+};
+
+const port = process.env.PORT || 3000;
+if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+  config.baseURL = `http://localhost:${port}`;
+}
+
+app.use(auth(config));
+
+app.get('/profile', requiresAuth(), (req, res, next) => {
+  res.status(200).json(req.oidc?.user);
+})
+
+
+app.get('/', (req, res) => {
+  const isAuthenticated = req.oidc?.isAuthenticated()
+  res.status(200).json({'success': isAuthenticated ? true : false});
 });
 
-app.listen(3000, () => {
+
+
+// Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+  res.locals.user = req.oidc.user;
+  next();
+});
+
+//catch route not found
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+//Error Handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: process.env.NODE_ENV !== 'production' ? err : {}
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is up on port 3000.');
 });
