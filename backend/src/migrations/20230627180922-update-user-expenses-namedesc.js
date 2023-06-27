10d7fca8703470d116f0e67a9475f9baf319d733
Aditya Narayan Mishra <mishraaditya384@outlook.com>
2023-06-28 00:56:45 +0530

    Sonar Code fix

--- backend/src/migrations/20230627180922-update-user-expenses-namedesc.js
+++ backend/src/migrations/20230627180922-update-user-expenses-namedesc.js
@@ -3,23 +3,29 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('User_Expenses', 'name', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    }),
+    await Promise.all([
+      await queryInterface.addColumn('User_Expenses', 'name', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }),
       await queryInterface.addColumn('User_Expenses', 'description', {
         type: Sequelize.STRING,
         allowNull: true,
+      }),
+      await queryInterface.addColumn('User_Expenses', 'modeOfExpense', {
+        type: Sequelize.ENUM('UPI', 'CASH', 'DEBIT CARD', 'CREDIT CARD'),
+        defaultValue: 'UPI',
       })
-    await queryInterface.addColumn('User_Expenses', 'modeOfExpense', {
-      type: Sequelize.ENUM('UPI', 'CASH', 'DEBIT CARD', 'CREDIT CARD'),
-      defaultValue: 'UPI',
-    })
+    ])
+
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('User_Expenses', 'name');
-    await queryInterface.removeColumn('User_Expenses', 'description');
-    await queryInterface.removeColumn('User_Expenses', 'modeOfExpense');
+    await Promise.all([
+      await queryInterface.removeColumn('User_Expenses', 'name'),
+      await queryInterface.removeColumn('User_Expenses', 'description'),
+      await queryInterface.removeColumn('User_Expenses', 'modeOfExpense')
+    ])
+
   }
 };
--- backend/src/migrations/20230627180945-update-user-countrycurradmin.js
+++ backend/src/migrations/20230627180945-update-user-countrycurradmin.js
@@ -3,28 +3,33 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'country', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    })
+    await Promise.all([
+      await queryInterface.addColumn('Users', 'country', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }),
 
-    await queryInterface.addColumn('Users', 'currency', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    })
+      await queryInterface.addColumn('Users', 'currency', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }),
 
-    await queryInterface.addColumn('Users', 'isAdmin', {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    })
+      await queryInterface.addColumn('Users', 'isAdmin', {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      })
+    ])
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'country')
+    await Promise.all([
+      await queryInterface.removeColumn('Users', 'country'),
 
-    await queryInterface.removeColumn('Users', 'currency')
+      await queryInterface.removeColumn('Users', 'currency'),
 
-    await queryInterface.removeColumn('Users', 'isAdmin')
+      await queryInterface.removeColumn('Users', 'isAdmin')
+    ])
+
   }
 };
